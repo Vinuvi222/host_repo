538c91263ddf3c87cb8dc74ef9214514b8849867
vinuvi-thalagala <vinuvithalagala01@outlook.com>
2026-01-13 20:22:50 +0530

    remove database connection

--- Backend/server.js
+++ Backend/server.js
@@ -21,14 +21,7 @@ app.use(express.json());
 dotenv.config();
 
 const PORT = process.env.PORT || 8070;
-const URL = process.env.MONGODB_URL;
-
-mongoose.connect(URL);
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB connection is successful');
-});
+ 
 
 // Routes
 app.use('/driver', driverRoutes);
